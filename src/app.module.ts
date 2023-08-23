ee3d0433cfca4c3abdd4a898d2ca587e74c39407
luis <luiszkm@gmail.com>
2023-08-23 10:24:51 -0300

    feat:add gateway websocket and Bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { RoutesModule } from './routes/routes.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { MapsModule } from './maps/maps.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),RoutesModule, PrismaModule, MapsModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    RoutesModule,
+    BullModule.forRoot({
+      redis: {
+        host: 'redis',
+        port: 6379,
+      },
+    }),
+     PrismaModule, 
+     MapsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
